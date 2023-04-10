b48083bb3320832836fb4590a3766397a087fb1a
Ivan Isakov <60196280+ivanisakov99@users.noreply.github.com>
2023-04-10 10:01:08 -0400

    Add `Which` command
    Equivalent to `which` in bash

--- Modules/Console-Utils/Console-Utils.psm1
+++ Modules/Console-Utils/Console-Utils.psm1
@@ -1,3 +1,7 @@
+# Utilities to manage PowerShell Consoles
+
+function Which($name) { Get-Command $name -ErrorAction SilentlyContinue | Select-Object Definition }
+
 function Test-Elevated {
     # Get the ID and security principal of the current user account
     $myIdentity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
@@ -6,4 +10,4 @@ function Test-Elevated {
     return $myPrincipal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
 }
 
-Export-ModuleMember -Function Test-Elevated
+Export-ModuleMember -Function Test-Elevated, Which
